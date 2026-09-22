fe303473aea488b967cad96c00c413ad8d997af9
Scorpascal <229578631+Scorpascal@users.noreply.github.com>
2026-09-22 09:56:28 +0800

    finish

--- bits.c
+++ bits.c
@@ -50,7 +50,7 @@ int bitXor(int x, int y) {
  *   1 if x and y have the same sign , 0 otherwise.
  */
 int samesign(int x, int y) {
-    return !((x >> 31) ^ (y >> 31)) & !(!x ^ !y);
+    return !((x ^ y) >> 31) & !(!x ^ !y);
 }
 
 /*
